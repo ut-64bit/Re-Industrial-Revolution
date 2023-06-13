1dc8d7514e82bff580c561748904de2803361d78
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-13 14:09:15 +0900

    script update

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -4,11 +4,6 @@ onEvent('jei.hide.items', event => {
 	//oldguns
 	event.hide('oldguns:iron_with_coal')
 
-	// botania
-	event.hide('botania:flight_tiara')
-	event.hide('botania:mana_fluxfield')
-	event.hide('botania:conjuration_catalyst')
-
 	// tconstruct
 	event.hide('tconstruct:earth_slime_sling')
 	event.hide('tconstruct:ender_slime_sling')
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -11,23 +11,31 @@ onEvent('recipes', event => {
 	event.recipes.shapeless(`oldguns:steel_ingot`, `#forge:ingots/steel`)
 	event.recipes.shapeless(`immersiveengineering:ingot_steel`, `#forge:ingots/steel`)
 
+	// minecraft
+	let removeTools = (material) => {
+		event.remove({ output: 'minecraft:' + material + '_pickaxe' })
+		event.remove({ output: 'minecraft:' + material + '_axe' })
+		event.remove({ output: 'minecraft:' + material + '_shovel' })
+		event.remove({ output: 'minecraft:' + material + '_hoe' })
+	}
+	removeTools('wooden')
+	removeTools('stone')
+	removeTools('iron')
+	removeTools('golden')
+	removeTools('diamond')
+
 	// oldguns
 	event.remove({ output: 'oldguns:steel_ingot' })
 	event.remove({ output: 'oldguns:iron_with_coal' })
 	event.replaceInput({ input: 'oldguns:steel_ingot' }, `oldguns:steel_ingot`, `#forge:ingots/steel`)
 	event.replaceInput({ input: 'oldguns:steel_block' }, `oldguns:steel_block`, `#forge:storage_blocks/steel`)
 
-	// botania
-	event.remove({ output: 'botania:flight_tiara' })
-	event.remove({ output: 'botania:mana_fluxfield' })
-	event.remove({ output: 'botania:conjuration_catalyst' })
-
 	// tconstruct
+	event.remove({ type: 'tconstruct:ore_melting' })
 	event.remove({ output: 'tconstruct:earth_slime_sling' })
 	event.remove({ output: 'tconstruct:ender_slime_sling' })
 	event.remove({ output: 'tconstruct:ichor_slime_sling' })
 	event.remove({ output: 'tconstruct:sky_slime_sling' })
-	event.remove({ type: 'tconstruct:ore_melting' })
 	event.remove({ id: 'tconstruct:smeltery/seared/grout' })
 	event.remove({ id: 'tconstruct:smeltery/seared/grout_multiple' })
 
@@ -42,3 +50,11 @@ onEvent('item.tags', event => {
 	event.add('forge:ingots/steel', 'oldguns:steel_ingot')
 	event.add('forge:storage_blocks/steel', 'oldguns:steel_ingot')
 })
+
+onEvent("lootjs", (event) => {
+	event.addLootTableModifier('minecraft:blocks/grass')
+		.removeLoot('minecraft:wheat_seeds');
+
+	event.addLootTableModifier('minecraft:blocks/tall_grass')
+		.removeLoot('minecraft:wheat_seeds');
+});
